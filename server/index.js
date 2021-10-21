4e86d3429ea942e82c928cf1ba5c1fe16605788d
Garden <kamil.ogrodowczyk111@gmail.com>
2021-10-21 17:58:20 +0200

    eight commit

--- server/index.js
+++ server/index.js
@@ -1,39 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-require('./db');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+require("./db");
+const path = require("path");
 
-const companyRouter = require('./routers/company');
-const userRouter = require('./routers/user');
-const eventRouter = require('./routers/event');
+const companyRouter = require("./routers/company");
+const userRouter = require("./routers/user");
+const eventRouter = require("./routers/event");
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-// app.use(express.static(path.join(__dirname, '..', 'build')));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static(path.join(__dirname, "..", "calendar", "build")));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 app.use(companyRouter);
 app.use(eventRouter);
 app.use(userRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('calendar/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("calendar/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'calendar', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "calendar", "build", "index.html"));
   });
 }
 
-app.get('/', (req, res) => {
-  res.send('<h2>This is from index.js file</h2>');
+app.get("/", (req, res) => {
+  res.send("<h2>This is from index.js file</h2>");
 });
 
-// app.use((req, res, next) => {
-//   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-// });
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "calendar", "build", "index.html"));
+});
+
+console.log(__dirname);
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
