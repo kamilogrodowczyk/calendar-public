5a701918865624ae43a542a1ec8f99bdaa17994b
Garden <kamil.ogrodowczyk111@gmail.com>
2021-10-21 19:26:21 +0200

    problem with db

--- server/db.js
+++ server/db.js
@@ -6,7 +6,6 @@ mongoose
       "mongodb+srv://calendar:calendar@cluster0.a85ct.mongodb.net/calendar",
     {
       useNewUrlParser: true,
-      useCreateIndex: true,
       useUnifiedTopology: true,
     },
   )
